789e16e11a32c293b63843a490b579646fe87ee2
chkgk <koenig.kersting@gmail.com>
2024-02-21 11:10:03 +0100

    demographics

--- demographics/__init__.py
+++ demographics/__init__.py
@@ -6,9 +6,51 @@ doc = """
 Your app description
 """
 
+
 def _(s):
     LANGUAGE_MAP["de"] = {
-        
+        "female": "weiblich",
+        "male": "männlich",
+        "other / I prefer not to tell": "anderes / möchte ich nicht sagen",
+        "What is your gender?": "Was ist Ihr Geschlecht?",
+        "What is your age?": "Wie alt sind Sie?",
+        "What is your field of studies?": "Was studieren Sie?",
+        "Economics": "Volkswirtschaftslehre",
+        "Business Administration": "Betriebswirtschaftslehre",
+        "Other Social Sciences": "Andere Sozialwissenschaften",
+        "Psychology": "Psychologie",
+        "Humanities": "Geisteswissenschaften",
+        "Natural Sciences": "Naturwissenschaften",
+        "Other": "Andere",
+        "How many semesters have you completed so far?": "Wie viele Semester haben Sie bisher absolviert?",
+        "How would you rate your overall mood today?": "Wie würden Sie Ihre Stimmung heute insgesamt bewerten?",
+        "very bad": "sehr schlecht",
+        "bad": "schlecht",
+        "neutral": "neutral",
+        "good": "gut",
+        "very good": "sehr gut",
+        "How would you rate your mood during the experiment?": "Wie würden Sie Ihre Stimmung während des Experiments bewerten?",
+        "How would you rate your level of exhaustion on a scale from 1 to 6?": "Wie würden Sie Ihre Erschöpfung auf einer Skala von 1 bis 6 bewerten?",
+        "1 - not exhausted at all": "1 - überhaupt nicht erschöpft",
+        "6 - very exhausted": "6 - sehr erschöpft",
+        "How strenuous do you think today’s experiment was on a scale from 1 to 6?": "Wie anstrengend denken Sie, dass das Experiment heute auf einer Skala von 1 bis 6 war?",
+        "1 - not strenuous at all": "1 - überhaupt nicht anstrengend",
+        "6 - very strenuous": "6 - sehr anstrengend",
+        "How many of the questions of Part 3 Task 3.1 did you know already?": "Wie viele der Fragen von Teil 3 Aufgabe 3.1 kannten Sie bereits?",
+        "How often have you participated in experiments?": "Wie oft haben Sie an Experimenten teilgenommen?",
+        "never before": "noch nie",
+        "once before": "einmal",
+        "2-5 times": "2-5 mal",
+        "more often": "öfter",
+        "How many of the other participants in the room do you know personally (approximately)?": "Wie viele der anderen Teilnehmer im Raum kennen Sie persönlich (ungefähr)?",
+        "How would you rate your risk appetite on a scale from 1 to 6?": "Wie würden Sie Ihre Risikobereitschaft auf einer Skala von 1 bis 6 bewerten?",
+        "1 - very risk averse": "1 - sehr risikoavers",
+        "6 - very risk seeking": "6 - sehr risikofreudig",
+        "What was your math grade at the end of school?": "Wie war Ihre Mathematiknote am Ende der Schule?",
+        "satisfactory": "befriedigend",
+        "sufficient": "ausreichend",
+        "deficient": "mangelhaft",
+        "inadequate": "ungenügend"
     }
     if LANGUAGE_CODE in LANGUAGE_MAP.keys():
         return LANGUAGE_MAP[LANGUAGE_CODE][s]
@@ -30,7 +72,78 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    pass
+    # demographics
+    gender = models.IntegerField(choices=[(1, _("female")), (2, _("male")), (3, _("other / I prefer not to tell"))],
+                                 widget=widgets.RadioSelect, label=_("What is your gender?"))
+    age = models.IntegerField(min=18, max=110, label=_("What is your age?"))
+    field_of_studies = models.IntegerField(choices=[
+        (1, _("Economics")),
+        (2, _("Business Administration")),
+        (3, _("Other Social Sciences")),
+        (4, _("Psychology")),
+        (5, _("Humanities")),
+        (6, _("Natural Sciences")),
+        (7, _("Other"))
+    ], widget=widgets.RadioSelect, label=_("What is your field of studies?"))
+    num_semesters = models.IntegerField(min=0, label=_("How many semesters have you completed so far?"))
+
+    math_grade = models.IntegerField(choices=[
+        (1, _("very good")),
+        (2, _("good")),
+        (3, _("satisfactory")),
+        (4, _("sufficient")),
+        (5, _("deficient")),
+        (6, _("inadequate"))
+    ], widget=widgets.RadioSelect, label=_("What was your math grade at the end of school?"))
+
+    risk_appetite = models.IntegerField(choices=[
+        (1, _("1 - very risk averse")),
+        (2, "2"),
+        (3, "3"),
+        (4, "4"),
+        (5, "5"),
+        (6, _("6 - very risk seeking")),
+    ], widget=widgets.RadioSelect, label=_("How would you rate your risk appetite on a scale from 1 to 6?"))
+
+    # questionnaire
+    mood_today = models.IntegerField(choices=[
+        (1, _("very bad")),
+        (2, _("bad")),
+        (3, _("neutral")),
+        (4, _("good")),
+        (5, _("very good"))
+    ], widget=widgets.RadioSelect, label=_("How would you rate your overall mood today?"))
+    mood_experiment = models.IntegerField(choices=[
+        (1, _("very bad")),
+        (2, _("bad")),
+        (3, _("neutral")),
+        (4, _("good")),
+        (5, _("very good"))
+    ], widget=widgets.RadioSelect, label=_("How would you rate your mood during the experiment?"))
+    exhaustion = models.IntegerField(choices=[
+        (1, _("1 - not exhausted at all")),
+        (2, "2"),
+        (3, "3"),
+        (4, "4"),
+        (5, "5"),
+        (6, _("6 - very exhausted")),
+    ], widget=widgets.RadioSelect, label=_("How would you rate your level of exhaustion on a scale from 1 to 6?"))
+    strenuousness = models.IntegerField(choices=[
+        (1, _("1 - not strenuous at all")),
+        (2, "2"),
+        (3, "3"),
+        (4, "4"),
+        (5, "5"),
+        (6, _("6 - very strenuous")),
+    ], widget=widgets.RadioSelect, label=_("How strenuous do you think today’s experiment was on a scale from 1 to 6?"))
+    num_crt_known = models.IntegerField(min=0, label=_("How many of the questions of Part 3 Task 3.1 did you know already?"))
+    num_previous_participation = models.IntegerField(choices=[
+        (0, _("never before")),
+        (1, _("once before")),
+        (2, _("2-5 times")),
+        (3, _("more often"))
+    ], label=_("How often have you participated in experiments?"))
+    num_known_participants = models.IntegerField(min=0, label=_("How many of the other participants in the room do you know personally (approximately)?"))
 
 
 # PAGES
@@ -38,8 +151,14 @@ class Part4Announcement(TranslatedPage):
     pass
 
 
-class Demographics1(TranslatedPage):
-    pass
+class Demographics(TranslatedPage):
+    form_model = 'player'
+    form_fields = ["gender", "age", "field_of_studies", "num_semesters", "math_grade", "risk_appetite"]
+
+
+class Questionnaire(TranslatedPage):
+    form_model = 'player'
+    form_fields = ["mood_today", "mood_experiment", "exhaustion", "strenuousness", "num_crt_known", "num_previous_participation", "num_known_participants"]
 
 
 class Payments(TranslatedPage):
@@ -48,7 +167,7 @@ class Payments(TranslatedPage):
 
 page_sequence = [
     Part4Announcement,
-    Demographics1,
-    # Demographics2,
+    Demographics,
+    Questionnaire,
     Payments
 ]
