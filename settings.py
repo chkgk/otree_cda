9788aa776c1d708bb4415e8ff1c8ace0dc489a48
chkgk <koenig.kersting@gmail.com>
2024-02-27 14:13:40 +0100

    repetition 2 announcement

--- settings.py
+++ settings.py
@@ -1,15 +1,15 @@
 from os import environ
 
 SESSION_CONFIGS = [
-    dict(
-         name="export_test",
-         display_name="Export Test",
-         app_sequence=["export_test"],
-         num_demo_participants=4,
-    ),
+    # dict(
+    #      name="export_test",
+    #      display_name="Export Test",
+    #      app_sequence=["export_test"],
+    #      num_demo_participants=4,
+    # ),
     dict(
         name="intro",
-        display_name="Introduction",
+        display_name="0 Introduction",
         app_sequence=["intro"],
         num_demo_participants=4,
     ),
@@ -40,7 +40,7 @@ SESSION_CONFIGS = [
     ),
     dict(
         name='cda_rep1',
-        display_name="2 Asset Market - Repetition 1",
+        display_name="2.1 Asset Market - Repetition 1",
         app_sequence=['kocher_cda'],  # 'cda_rep1'
         num_demo_participants=4,
         trading_seconds=120,
@@ -50,6 +50,12 @@ SESSION_CONFIGS = [
         endowment_high_cash=(3000, 20),
         endowment_low_cash=(1000, 60),
         repetition=1
+    ),
+    dict(
+        name="start_rep2",
+        display_name="2.2 Announcement of Repetition 2",
+        app_sequence=["start_rep2"],
+        num_demo_participants=4,
     ),
     # dict(
     #     name='cda_rep2',
@@ -96,6 +102,8 @@ SESSION_CONFIGS = [
             # "cda_practice",
             "stroop",
             "kocher_cda",
+            # "cda_rep1",
+            "start_rep2",
             # "cda_rep2",
             "crt7",
             "apm",
@@ -120,6 +128,7 @@ SESSION_CONFIGS = [
             "movie",
             "kocher_cda",
             # "cda_rep1",
+            "start_rep2",
             # "cda_rep2",
             "crt7",
             "apm",
@@ -151,7 +160,7 @@ SESSION_FIELDS = []
 
 # ISO-639 code
 # for example: de, fr, ja, ko, zh-hans
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'de'
 
 # e.g. EUR, GBP, CNY, JPY
 REAL_WORLD_CURRENCY_CODE = 'EUR'
