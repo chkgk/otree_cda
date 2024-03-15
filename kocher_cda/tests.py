f2a05090cef06698a6e2e002e4c5e1cdcd1633f1
chkgk <koenig.kersting@gmail.com>
2024-03-15 14:56:25 +0100

    settings, removed kocher_cda

--- kocher_cda/tests.py
+++
@@ -1,8 +0,0 @@
-from otree.api import Currency as c, currency_range, expect, Bot
-from . import Trading, TradingSummary
-
-
-class PlayerBot(Bot):
-    def play_round(self):
-        yield Trading
-        yield TradingSummary
